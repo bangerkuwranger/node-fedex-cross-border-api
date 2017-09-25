a77e235fc230450abdc4fbcb6dcc46961f6f6765
Chad A. Carino <ccarino@sundialbrands.com>
2017-09-25 18:27:41 -0400

    removed soap and added request; fecb requires more specific namespacing than soap handles (for arrays, specifically)... if I'm going to have to parse the crap out of it before sending anyhow, it doesn't make sense to load a heavy dep too. Added test item to sync test (going to rename that to avoid confusion... callback / promise is a more accurate nomenclature for the behavior here). Started unit tests for productInfo method, now have to move some of that logic over to performRequest method of the 'class'. Probably also going to subclass the methods to avoid clogging the base file with all of the logic... readability and all that. Tossed the wsdl file (ick) into the repo for user reference. Fixed some off by ones and stupid logical errors. TBD - update the docs in progress to align with the changes in output flags and nomenclature, as well as mentioning that FECB error codes return error object, not output. (output options are now default result, {body: {parsedBodyObject}, statusCode: 'response.statusCode', request: {productInfoRequestObject}}, stripWrapper result{parsedBodyObject}, returnRaw result+ raw:{response.body}, returnSoapHeader result+ soapHeader:'JSON.stringify(soapHeaders)', and returnFullResponse result+ response:{responseObject). Not sure if that's a useful improvement or not...

--- test/client.sync.class.js
+++ test/client.sync.class.js
@@ -2,14 +2,39 @@
 let chai = require('chai');
 chai.should();
 let ClientSync = require('../lib/index').client;
-describe('ClientSync', function() {
+let util = require('util');
+
+let testItem = {
+	id: 'testItem',
+	description: 'Item used for Unit Testing',
+	price: 9.9900,
+	currency: 'USD',
+	exportHub: 'TPA',
+	origin: 'US',
+	itemInformation: {
+		L: 10.0000,
+		W: 10.0000,
+		H: 10.0000,
+		WT: 1.0000
+	},
+	optionalArgs: {
+		productName: 'Test Item',
+		url: 'https://www.teststore.com/products/testitem',
+		imageUrl: 'https://www.teststore.com/images/products/testitem.jpg',
+		hazFlag: 0,
+		eccn: '2C001',
+		productType: 'Testing'
+	}
+};
+
+describe('ClientCallback', function() {
 
 	let clientSync;
 	beforeEach('init new client', function() {
 	
 		clientSync = new ClientSync({
 			language: 'En',
-			merchantKey: '1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*+'
+			merchantKey: '1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^=*+'
 		});
 	
 	});
@@ -25,6 +50,82 @@ describe('ClientSync', function() {
 		
 			clientSync.language = 'es';
 			clientSync.language.should.equal('es');
+		
+		});
+	
+	});
+	
+	describe('#merchantKey', function() {
+	
+		it('should not accept keys longer than 64 chars in constructor', function() {
+		
+			(function() {
+			
+				clientSync = new ClientSync({
+					language: 'en',
+					merchantKey: '1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*+.1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*+.'
+				});
+			
+			}).should.throw(TypeError, 'FECB client constructor requires args.merchantKey to be a string of 64 characters or fewer');
+		
+		});
+		
+	
+	});
+	
+	describe('#productInfo', function() {
+	
+		it('requires a valid callback function', function() {
+		
+			(function() {
+			
+				clientSync.productInfo([]);
+			
+			}).should.throw(TypeError, 'FECB client.productInfo was passed an invalid callback');
+		
+		});
+		
+		it('requires callback to have two arguments', function() {
+		
+			(function() {
+			
+				clientSync.productInfo([], function(error) { return; });
+			
+			}).should.throw(TypeError, 'FECB client.productInfo was passed an invalid callback');
+		
+		});
+		
+		it('requires the first argument to be an array with a length of one or more', function() {
+		
+			(function() {
+			
+				clientSync.productInfo([], function(error, result) { return; });
+			
+			}).should.throw(TypeError, 'FECB client.productInfo was passed an invalid array of items');
+		
+		});
+		
+		it('can connect to fecb api', function(done) {
+		
+			let testItems = [testItem];
+			clientSync.returnRaw = true;
+			clientSync.returnSoapHeader = true;
+// 			console.log(clientSync.merchantKey);
+			clientSync.productInfo(testItems, function(error, result) {
+// 				console.log(util.inspect(result, {depth: null}));
+				if (error) {
+				
+					error.message.should.equal('Code 1000 — <p>Invalid Partner Key</p>');
+					done();
+				
+				}
+				else {
+				
+					done(JSON.stringify(result));
+				
+				}
+			
+			});
 		
 		});
 	
